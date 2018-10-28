e3c34a3a932c02c18f76d692278aac3ea7923545
Anurag Dwivedi <40738395+noobcod3r@users.noreply.github.com>
2018-10-28 18:19:07 +0530

    GOLDMAN PRIME THEOREM
    O(n) solution of many problems

---
+++ goldman prime therem.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#define M 1000000007
+#define fr(i,c,b) for(int i=c;i<b;i++)
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a;
+        cin>>a;
+        if(a%2==0 && a!=2)
+        {
+            cout<<"Deepa"<<endl;
+        }
+        else
+        {
+            cout<<"Arjit"<<endl;
+        }
+
+    }
+}
